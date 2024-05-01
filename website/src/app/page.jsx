3bdbe0db95e140c198d07b75b8c7e5c41ac71650
Jordan Hamilton <jordanh6489@hotmail.com>
2024-05-01 07:22:14 -0400

    made some changes

---
+++ website/src/app/page.jsx
@@ -0,0 +1,12 @@
+import { Content } from "next/font/google";
+import './globals.css';
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 >Hello world!</h1>
+      </div>
+    </main>
+  );
+}

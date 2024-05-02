2df30455c0611a5fe459d1d293c5444c8e2aa425
Jordan Hamilton <jordanh6489@hotmail.com>
2024-05-02 06:30:55 -0400

    more landing page progress!

--- website/src/app/page.jsx
+++ website/src/app/page.jsx
@@ -1,6 +1,8 @@
 import { Content, Finlandica } from "next/font/google";
 import './globals.css';
+import './Pages/loginPage.jsx'
 
+//allows users to use dark mode (browser prefrence settings)
 function useDarkMode(){
   return(
     <div>
@@ -12,14 +14,41 @@ function useDarkMode(){
 
 export default function Home() {
   return (
-    <main className="Header">
+    <main>
+      <div className="Top">
+        <useDarkMode />
 
-      <useDarkMode />
-      
-      <div className="Header">
-        <h1>Jordan's Website</h1>
+        <nav className="Navigation">
+          <ul className="NavigationList">
+            <li className="NavItem">
+              <a href="https://www.youtube.com/@InboundG" target="blank">Youtube</a>  
+            </li> 
+            <li className="NavItem">
+              <a href="https://github.com/inbound-dev" target="blank">Github</a>
+            </li> 
+            <li className="NavItem">
+              <a href="https://www.linkedin.com/in/jordan-hamilton-dev/" target="blank">LinkedIn</a>
+            </li>
+            <li className="NavItem">
+              <a href="">Projects</a>
+            </li>
+            <li className="NavItem">
+              <a href="">Login</a>
+            </li>
+          </ul>  
+        </nav>
+
+        <div className="Header">
+          <h1>Jordan's Website</h1>
+        </div>
+
+        <p>I am a Programmer, Photographer, and Content Creator</p>
       </div>
+      {/*bottom of landing page*/}
 
+      <div className="Bottom">
+        <h1>Hello World!</h1>
+      </div>
     </main>
   );
 }
